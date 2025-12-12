04bfafb77d5905d028229b94e46ddb6469ee905f
Данил Правдин <pravdin@MacBook-Air-Danil.local>
2025-12-12 16:31:09 +0300

    deploy to main

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/danyalomin01-stack.github.io/",
+  base: "/",   // ← ВОТ ТАК ДОЛЖНО БЫТЬ
 });
